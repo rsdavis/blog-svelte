02b0c0b443230c9773f674e099af6e4a0bdbf998
Ryan Davis <ryanscdavis@gmail.com>
2020-06-20 12:47:25 -0400

    Add footer with links

---
+++ src/components/Footer.svelte
@@ -0,0 +1,31 @@
+
+<script>
+    import Fa from 'svelte-fa'
+    import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+</script>
+
+<div class='container'>
+    <a href='https://github.com/ryanscdavis'>
+        <Fa icon={faGithub} size='2x'/>
+    </a>
+    <a href='https://www.linkedin.com/in/ryanscdavis/'>
+        <Fa icon={faLinkedin} size='2x'/>
+    </a>
+</div>
+
+<style>
+    .container {
+        height: 80px;
+        display: flex;
+        color: var(--gray-dark);
+        align-items: center;
+    }
+
+    .container a {
+        margin-right: 30px;
+    }
+
+    .container a:hover {
+        color: var(--flash);
+    }
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import Nav from '../components/Nav.svelte'
+    import Footer from '../components/Footer.svelte'
     export const segment = null
 </script>
 
@@ -19,11 +20,20 @@
     </div>
 </main>
 
+<footer>
+    <div class='content'>
+        <Footer/>
+    </div>
+</footer>
 
 <style>
 
     header {
         border-bottom: 1px solid rgb(210,210,210);
+    }
+
+    footer {
+        border-top: 1px solid rgb(210,210,210);
     }
 
     .content {
@@ -33,7 +43,7 @@
     }
 
     main > .content {
-        margin-bottom: 50px;
+        margin-bottom: 80px;
     }
 
 </style>
